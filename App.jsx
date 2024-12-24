37e53330a158e7f55024b8cc077e4f42c90dbac0
Sumit Shah <sumitshah43621@gmail.com>
2024-12-24 20:18:33 +0530

    View, Button

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={ { fontSize:30} }>Hello World</Text>
+      <Text style={ { fontSize:30} }>Sumit Shah</Text>
+      <Button title='Press Here'/>
+    </View>
+  )
+}
+
+export default App
